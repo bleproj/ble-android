6ae4b4963cfdb1582f9ecbf46f3f1e440acdc50c
Harald Andersson <hairald666@gmail.com>
2013-11-26 10:59:21 +0100

    new structure

---
+++ src/com/ble/ble/CheckinActivity.java
@@ -0,0 +1,11 @@
+package com.ble.ble;
+
+import android.app.Activity;
+
+public class CheckinActivity extends Activity {
+
+	public CheckinActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
